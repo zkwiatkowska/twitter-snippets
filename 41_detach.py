2da700dffe311d93d5062f40e6a9225509cec116
zkwiatkowska <z.kwiatkowska7@gmail.com>
2022-01-25 21:40:10 +0100

    Fix in snippet 41

--- 41_detach.py
+++ 41_detach.py
@@ -1,13 +1,13 @@
 import torch
 
-x = torch.tensor([1.], requires_grad=True)
+x = torch.tensor([2.], requires_grad=True)
 
-y1 = x ** 2 + x - 1
+y1 = x ** 2 + 2 * x - 1
 y1.backward()
-print(x.grad)                                   # <-- this results in 3.
+print(x.grad)                                   # <-- this results in 6.
 
-x = torch.tensor([1.], requires_grad=True)
+x = torch.tensor([2.], requires_grad=True)
 
-y2 = x ** 2 + x.detach() - 1
+y2 = x ** 2 + 2 * x.detach() - 1
 y2.backward()
-print(x.grad)                                   # <-- this results in 2.
+print(x.grad)                                   # <-- this results in 4.
