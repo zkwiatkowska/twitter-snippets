16a72b82a6a172c146e15ae37b6c844a092292ea
zkwiatkowska <z.kwiatkowska7@gmail.com>
2022-02-08 17:00:07 +0100

    Snippet 50 update

--- 50_python_zip.py
+++ 50_python_zip.py
@@ -1,6 +1,10 @@
 names = ["Alice", "Bob", "Mark", "Frank", "Anna"]
 person_id = [14324, 78392, 94721, 40293, 58294]
 
-print(zip(names, person_id))
-print(list(zip(names, person_id)))
-print(dict(zip(names, person_id)))
+print(zip(names, person_id))           # <zip object at 0x7f88586ddf80>
+print(list(zip(names, person_id)))     # [('Alice', 14324), ('Bob', 78392), ('Mark', 94721), ...]
+print(dict(zip(names, person_id)))     # {'Alice': 14324, 'Bob': 78392, 'Mark': 94721, ...}
+
+zipped = zip(names, person_id)
+print(list(zipped))
+print(dict(zipped))
